c41399a4e6562e1ba0b328b12ee8edd72fc3709c
Yash Mungra <yashmungra75@gmail.com>
2026-09-08 14:52:42 +0530

    Add solution of 1343B

--- 1343B.cpp
+++ 1343B.cpp
@@ -12,20 +12,20 @@ int main()
 
         if ((n / 2) % 2 == 1)
         {
-            cout << "NO" << endl;
+            cout << "NO" << "\n";
         }
         else
         {
-            cout << "YES" << endl;
+            cout << "YES" << "\n";
             for (int i = 1; i <= n / 2; i++)
             {
-                cout << i * 2 << endl;
+                cout << i * 2 << "\n";
             }
             for (int i = 1; i < n / 2; i++)
             {
-                cout << (i * 2) - 1 << endl;
+                cout << i*2 - 1 << "\n";
             }
-            cout << n - 1 + (n / 2) << endl;
+            cout << n - 1 + n/2 << "\n";
         }
     }
     return 0;
